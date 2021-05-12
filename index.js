aa360cd087e69173d16008a148d86ed18fdca818
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-05-12 18:12:39 +0530

    wallet

--- index.js
+++ index.js
@@ -56,3 +56,21 @@ class Chain {
     }
 }
 Chain.instance = new Chain();
+class Wallet {
+    constructor() {
+        const keypair = crypto.generateKeyPairSync("rsa", {
+            modulusLength: 2048,
+            publicKeyEncoding: { type: 'spki', format: 'pem' },
+            privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+        });
+        this.publicKey = keypair.publicKey;
+        this.privateKey = keypair.privateKey;
+    }
+    sendMoney(amont, payeePublicKey) {
+        const transaction = new Transaction(amont, this.publicKey, payeePublicKey);
+        const sign = crypto.createSign('SHA256');
+        sign.update(transaction.toString()).end();
+        const signature = sign.sign(this.privateKey);
+        Chain.instance.addBlock(transaction, this.publicKey, signature);
+    }
+}
--- index.ts
+++ index.ts
@@ -46,6 +46,33 @@ class Chain {
     addBlock(transaction: Transaction, senderPublicKey:string,signature:Buffer ){
         const newBlock = new Block(this.lastblock.hash,transaction);
         this.chain.push(newBlock);
+
+    }
+}
+
+
+
+class Wallet {
+    public publicKey:string;
+    public privateKey:string;
+    constructor() {
+        const keypair = crypto.generateKeyPairSync( "rsa" ,{
+            modulusLength: 2048,
+            publicKeyEncoding: { type: 'spki',format:'pem' },
+            privateKeyEncoding: { type: 'pkcs8',format:'pem' },
+        })
+
+        this.publicKey = keypair.publicKey;
+        this.privateKey = keypair.privateKey;
+    }
+
+    sendMoney(amont:number,payeePublicKey:string){
+        const transaction = new Transaction(amont,this.publicKey,payeePublicKey);
+
+        const sign = crypto.createSign('SHA256');
+        sign.update(transaction.toString()).end();
         
+        const signature = sign.sign(this.privateKey);
+        Chain.instance.addBlock(transaction,this.publicKey,signature);
     }
 }
